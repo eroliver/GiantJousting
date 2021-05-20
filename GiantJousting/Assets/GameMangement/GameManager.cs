7142d83392f9a76bfe89360fe1034a8d667df05f
Eric Oliver <eroliver27@hotmail.com>
2021-05-20 07:18:42 -0400

    Added game manager
    Singleton game manager script will handle all other managers, like scene loader score manager and unlockable manager.

---
+++ GiantJousting/Assets/GameMangement/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gameManager;
+
+    private void Awake()
+    {
+        MakeSingleton();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void MakeSingleton()
+    {
+        if(gameManager != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            gameManager = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
