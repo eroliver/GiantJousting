d1eb948532b3cda0d59af659d34e237b62274867
eroliver <eroliver27@hotmail.com>
2021-05-24 16:03:24 -0400

    Started animating attacks and blocking

--- GiantJousting/Assets/AnimationStateController.cs
+++ GiantJousting/Assets/AnimationStateController.cs
@@ -7,12 +7,14 @@ public class AnimationStateController : MonoBehaviour
     Animator animator;
     //store is walking string as the simpler data type hash
     int isWalkingHash;
+    int isBlockingHash;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         isWalkingHash = Animator.StringToHash("isWalking");
+        isBlockingHash = Animator.StringToHash("Great Sword Blocking");
     }
 
     // Update is called once per frame
@@ -20,7 +22,7 @@ public class AnimationStateController : MonoBehaviour
     {
         bool isWalking = animator.GetBool(isWalkingHash);
         bool forwardPressed = Input.GetKey("w");
-
+        
         if (!isWalking && forwardPressed)
         {
             animator.SetBool("isWalking", true);
@@ -29,5 +31,21 @@ public class AnimationStateController : MonoBehaviour
         {
             animator.SetBool("isWalking", false);
         }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            animator.Play("Great Sword Slash");
+        }
+
+        if (Input.GetMouseButtonDown(2))
+        {
+            animator.SetTrigger("Block");
+        }
+
+        if (Input.GetMouseButtonDown(2))
+        {
+            animator.ResetTrigger("Block");
+        }
+
     }
 }
