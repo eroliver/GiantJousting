f3234b2bb80285d1fc450abeec8891da20d5de40
Eric Oliver <eroliver27@hotmail.com>
2021-05-24 12:31:07 -0400

    Added mixamo assets and began animation
    Ybot asset and corresponding animations added to start prototyipng physics based motion

---
+++ GiantJousting/Assets/AnimationStateController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationStateController : MonoBehaviour
+{
+    Animator animator;
+    //store is walking string as the simpler data type hash
+    int isWalkingHash;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        isWalkingHash = Animator.StringToHash("isWalking");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isWalking = animator.GetBool(isWalkingHash);
+        bool forwardPressed = Input.GetKey("w");
+
+        if (!isWalking && forwardPressed)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        if (isWalking && !forwardPressed)
+        {
+            animator.SetBool("isWalking", false);
+        }
+    }
+}
